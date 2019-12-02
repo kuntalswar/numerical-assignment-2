556eb2a5c82c3deb9d73c0a88a75a40941253cfd
kuntalswar <56844967+kuntalswar@users.noreply.github.com>
2019-12-03 00:55:59 +0530

    Add files via upload

---
+++ sigeof.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+	printf("size of char is %lu\n",sizeof(char));
+	printf("size of int is %lu\n",sizeof(int));
+	printf("size of long int is %lu\n",sizeof(long int));
+	printf("size of float is %lu\n",sizeof(float));
+	printf("size of double is %lu\n",sizeof(double));
+	return 0;
+}
